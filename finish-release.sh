4262cdf1250fc5da6e4230df6507f10b4a86acc3
raulanatol <raul@natol.es>
2021-03-22 18:41:16 +0000

    Scripts and fixes

---
+++ finish-release.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -eu
+
+function error() {
+  echo "🚨 Error: $1"
+  exit 1
+}
+
+if [[ $# != 1 ]]; then
+  error "Please specify the version number: npm run finish-release 10.0.1"
+fi
+
+VERSION_PARAM=$1
+
+function change_version() {
+  NEW_VERSION=$(npm version "${VERSION_PARAM}")
+  echo "::set-output tagName=v${NEW_VERSION}"
+}
+
+change_version
